9281362490a9072c36cc6aab34dac1de1274285a
Furkan KAYA <97371955+furkankayam@users.noreply.github.com>
2025-10-31 09:49:43 +0300

    Document usage of @HttpExchange in PersonClient
    Added comments explaining usage of @HttpExchange annotation.

--- spring-backend/src/main/java/com/example/demo/client/PersonClient.java
+++ spring-backend/src/main/java/com/example/demo/client/PersonClient.java
@@ -5,8 +5,17 @@ import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
 @HttpExchange
+// @HttpExcahnge iki sekil kullanim alani var.
+// 1.
+// Bütün metotlar "/persons" yolu ile başlayacak
+// @HttpExchange("/persons")
 public interface PersonClient {
 
     @GetExchange("/persons")
     PersonDTO getPerson();
+
+    // 2.
+    // HTTP metodu ve path aynı anda belirtilir
+    // @HttpExchange(method = "GET", url = "/persons") 
+    //Person getPerson();
 }
